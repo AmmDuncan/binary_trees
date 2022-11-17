f64f795b520d2ca95fbd2ced210c7fb27b740c0a
ammduncan <ammielgyanyawson@gmail.com>
2022-11-17 09:28:23 +0000

    fix 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,10 +10,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	if (parent == NULL)
-		return (NULL);
-
 	node = malloc(sizeof(binary_tree_t));
+	if (parent == NULL || node == NULL)
+		return (NULL);
 	node->n = value;
 	node->parent = parent;
 	node->left = parent->left;
