32ed456dacda9a370b5d67362c40321f5c10c60c
ammduncan <ammielgyanyawson@gmail.com>
2022-11-18 10:15:48 +0000

    fix finish

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -8,23 +8,23 @@
  */
 avl_t *array_to_avl(int *array, size_t size)
 {
-  size_t i, j;
-	
+	size_t i, j;
 	avl_t *tree = NULL;
+
 	if (array == NULL)
-	  return (NULL);
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
-	  for (j = 0; j < i; j++)
-	    {
-	      if (array[j] == array[i])
-		break;
-	    }
-	  if (j == i)
-	    {
-	      if (avl_insert(&tree, array[i]) == NULL)
-		return (NULL);
-	    }
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] == array[i])
+				break;
+		}
+		if (j == i)
+		{
+			if (avl_insert(&tree, array[i]) == NULL)
+				return (NULL);
+		}
 	}
 	return (tree);
 }
--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -1,113 +1,143 @@
 #include "binary_trees.h"
 
+/**
+ * avl_remove - avl_remove
+ *
+ * @root: root
+ * @value: value
+ * Return: avl_t*
+ */
 avl_t *avl_remove(avl_t *root, int value)
 {
-  avl_t *root_a = (avl_t *) bst_remove((bst_t *) root, value);
+	avl_t *root_a = (avl_t *)bst_remove((bst_t *)root, value);
 
-  if (root_a == NULL)
-    return (NULL);
-  bal(&root_a);
-  return (root_a);
-
+	if (root_a == NULL)
+		return (NULL);
+	bal(&root_a);
+	return (root_a);
 }
 
+/**
+ * bst_remove - bst_remove
+ *
+ * @root: root
+ * @value: value
+ * Return: avl_t*
+ */
 bst_t *bst_remove(bst_t *root, int value)
 {
-  int type = 0;
+	int type = 0;
 
-  if (root == NULL)
-    return (NULL);
-  if (value < root->n)
-    bst_remove(root->left, value);
-  else if (value > root->n)
-    bst_remove(root->right, value);
-  else if (value == root->n)
-    {
-      type = remove_type(root);
-      if (type != 0)
-	bst_remove(root->right, type);
-    }
-  else
-    return (NULL);
-  return (root);
+	if (root == NULL)
+		return (NULL);
+	if (value < root->n)
+		bst_remove(root->left, value);
+	else if (value > root->n)
+		bst_remove(root->right, value);
+	else if (value == root->n)
+	{
+		type = remove_type(root);
+		if (type != 0)
+			bst_remove(root->right, type);
+	}
+	else
+		return (NULL);
+	return (root);
 }
 
+/**
+ * remove_type - remove_type
+ *
+ * @root: root
+ * Return: int
+ */
 int remove_type(bst_t *root)
 {
-  int new_value = 0;
+	int new_value = 0;
 
-  if(!root->left && !root->right)
-    {
-      if (root->parent->right == root)
-	root->parent->right = NULL;
-      else
-	root->parent->left = NULL;
-      free(root);
-      return (0);
-    }
-  else if ((!root->left && root->right) || (!root->right && root->left))
-    {
-      if (!root->left)
+	if (!root->left && !root->right)
 	{
-	  if (root->parent->right == root)
-	    root->parent->right = root->right;
-	  else
-	    root->parent->left = root->left;
-
-	  root->right->parent = root->parent;
+		if (root->parent->right == root)
+			root->parent->right = NULL;
+		else
+			root->parent->left = NULL;
+		free(root);
+		return (0);
 	}
-      if (!root->right)
+	else if ((!root->left && root->right) || (!root->right && root->left))
 	{
-	  if(root->parent->right == root)
-	    root->parent->right = root->left;
-	  else
-	    root->parent->left = root->left;
+		if (!root->left)
+		{
+			if (root->parent->right == root)
+				root->parent->right = root->right;
+			else
+				root->parent->left = root->left;
 
-	  root->left->parent = root->parent;
+			root->right->parent = root->parent;
+		}
+		if (!root->right)
+		{
+			if (root->parent->right == root)
+				root->parent->right = root->left;
+			else
+				root->parent->left = root->left;
+
+			root->left->parent = root->parent;
+		}
+		free(root);
+		return (0);
 	}
-      free(root);
-      return (0);
-    }
-  else
-    {
-      new_value = successor(root->right);
-      root->n = new_value;
-      return (new_value);
-    }
+	else
+	{
+		new_value = successor(root->right);
+		root->n = new_value;
+		return (new_value);
+	}
 }
 
+/**
+ * successor - successor
+ *
+ * @node: node
+ * Return: int
+ */
 int successor(bst_t *node)
 {
-  int left = 0;
+	int left = 0;
 
-  if (node == NULL)
-    {
-      return (0);
-    }
-  else
-    {
-      left = successor(node->left);
-      if (left == 0)
+	if (node == NULL)
 	{
-	  return (node->n);
+		return (0);
 	}
-      return (left);
-    }
+	else
+	{
+		left = successor(node->left);
+		if (left == 0)
+		{
+			return (node->n);
+		}
+		return (left);
+	}
 }
 
+/**
+ * bal - bal
+ *
+ * @tree: tree
+ */
 void bal(avl_t **tree)
 {
-  int bval;
+	int bval;
 
-  if (tree == NULL || *tree == NULL)
-    return;
-  if ((*tree)->left == NULL && (*tree)->right == NULL)
-    return;
-  bal(&(*tree)->left);
-  bal(&(*tree)->right);
-  bval = binary_tree_balance((const binary_tree_t *)*tree);
-  if (bval > 1)
-    *tree = binary_tree_rotate_right((binary_tree_t *)*tree);
-  else if (bval < -1)
-    *tree = binary_tree_rotate_left((binary_tree_t *)*tree);
+	if (tree == NULL || *tree == NULL)
+		return;
+	if ((*tree)->left == NULL && (*tree)->right == NULL)
+		return;
+	bal(&(*tree)->left);
+	bal(&(*tree)->right);
+	bval = binary_tree_balance((const binary_tree_t *)*tree);
+	if (bval > 1)
+		*tree = binary_tree_rotate_right((binary_tree_t *)*tree);
+	else if (bval < -1)
+		*tree = binary_tree_rotate_left((binary_tree_t *)*tree);
 }
